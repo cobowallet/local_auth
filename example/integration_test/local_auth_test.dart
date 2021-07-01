6cf2eb2d0e7181c297e7d1f89f76ded9941836d7
wufd <dong.wu@cobo.com>
2021-07-01 15:04:45 +0800

    rebase 1.1.6

--- example/integration_test/local_auth_test.dart
+++ example/integration_test/local_auth_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
@@ -7,6 +11,9 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('canCheckBiometrics', (WidgetTester tester) async {
-    expect(LocalAuthentication().getAvailableBiometrics(), completion(isList));
+    expect(
+      LocalAuthentication().getAvailableBiometrics(),
+      completion(isList),
+    );
   });
 }
--- example/test_driver/integration_test.dart
+++ example/test_driver/integration_test.dart
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <Flutter/Flutter.h>
-#import <UIKit/UIKit.h>
+import 'package:integration_test/integration_test_driver.dart';
 
-@interface AppDelegate : FlutterAppDelegate
-
-@end
+Future<void> main() => integrationDriver();
